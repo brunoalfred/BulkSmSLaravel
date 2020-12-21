0b70a43b7eeaf88228045f929c74fd163c444798
bruno alfred <alfredybruno@gmail.com>
2020-12-21 10:52:29 +0300

    sms controler

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,4 @@ Auth::routes();
 
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/sms', [App\Http\Controllers\BroadcastSms::class, 'Send' ]);
+Route::get('/sms', [App\Http\Controllers\BroadcastSms::class, 'Send' ])->name('sms');
