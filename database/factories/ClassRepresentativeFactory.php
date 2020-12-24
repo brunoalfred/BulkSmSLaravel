cf9c8150a5f877d4b2ad3d715936b0c17eec9f50
bruno alfred <alfredybruno@gmail.com>
2020-12-24 20:22:10 +0300

    Feat(WIP): Created ClassRepresentativesfactory

---
+++ database/factories/ClassRepresentativeFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\ClassRepresentative;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ClassRepresentativeFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = ClassRepresentative::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
