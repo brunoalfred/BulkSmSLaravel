ca7caab557a372126da3a95c0395dd997819d8a4
bruno alfred <alfredybruno@gmail.com>
2020-12-24 19:52:20 +0300

    (WIP)Feat: Error Page 419

--- resources/views/errors/419.blade.php
+++ resources/views/errors/419.blade.php
@@ -26,8 +26,8 @@
                 <div class="text-center">
                     <img src="assets/images/file-searching.svg" height="90" alt="File not found Image">
 
-                    <h1 class="text-error mt-4">404</h1>
-                    <h4 class="text-uppercase text-danger mt-3">Page Not Found</h4>
+                    <h1 class="text-error mt-4">419</h1>
+                    <h4 class="text-uppercase text-danger mt-3">Page Expired</h4>
                     <p class="text-muted mt-3">It's looking like you may have taken a wrong turn. Don't worry... it
                         happens to the best of us. Here's a
                         little tip that might help you get back on track.</p>
