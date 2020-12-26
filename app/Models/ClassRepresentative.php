4b2de5cae2292207bc70105d84ac8fc5df2924d5
bruno alfred <alfredybruno@gmail.com>
2020-12-26 12:23:29 +0300

    Data Table view

--- app/Models/ClassRepresentative.php
+++ app/Models/ClassRepresentative.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model as Model;
 
 
@@ -19,6 +20,8 @@ use Illuminate\Database\Eloquent\Model as Model;
  */
 class ClassRepresentative extends Model
 {
+    use HasFactory;
+
 
     public $table = 'class_representatives';
 
